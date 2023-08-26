c25611925906de0e082f840b096593383eccd7e9
AZhDevelop <azh.develop@mail.ru>
2023-08-26 15:48:31 +0300

    Some improvements to NavLink + Logo NavLinK

--- react_frontend/src/components/navigation/navigation.jsx
+++ react_frontend/src/components/navigation/navigation.jsx
@@ -3,25 +3,20 @@ import style from './navigation.module.css'
 import { NavLink } from 'react-router-dom'
 
 const active_style = ({isActive}) => isActive ? style.active : style.non_active
+const logo_active_style = ({isActive}) => isActive ? style.logo : style.logo
 
 const Navigation = () => {
     return (
         <div className={style.navigation}>
-            <div className={style.logo}>
+            <NavLink to='/' className={logo_active_style}>
                 <img src={logo} alt='Logo'></img>
                 <h3>AZhDevelop</h3>
-            </div>
+            </NavLink>
             <br></br>
             <nav className={style.menu}>
-                <NavLink to="/" className={active_style}>
-                        SR Staff
-                </NavLink>
-                <NavLink to="/functions" className={active_style}>
-                        SR Functions
-                </NavLink>
-                <NavLink to="/improvements" className={active_style}>
-                        SR Improvements
-                </NavLink>
+                <NavLink to="/" className={active_style}>SR Staff</NavLink>
+                <NavLink to="/functions" className={active_style}>SR Functions</NavLink>
+                <NavLink to="/improvements" className={active_style}>SR Improvements</NavLink>
             </nav>
         </div>
     );
