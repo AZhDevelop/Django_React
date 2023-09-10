97e2bb293668be74d1498842882a7206ccaf5d08
AZhDevelop <azh.develop@mail.ru>
2023-09-10 19:42:01 +0300

    Returned to SQLite database

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -85,7 +85,10 @@ WSGI_APPLICATION = 'django_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': ServerConfig.postgres_config
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
